aa960e5180de80db1af85cc96286c9fc74b8d5f3
Alex Papanikos <apapanikos@convertgroup.com>
2022-10-04 22:51:48 +0300

    first commit

---
+++ components/AlbumCard.tsx
@@ -0,0 +1,12 @@
+export default function AlbumCard({album}) {
+  return (
+    <li class="group active:shadow-md transition duration-200 ease-in-out cursor-pointer my-2 p-2 rounded border-2 border-gray-500 hover:bg-white hover:shadow-xl hover:border-transparent">
+      <a
+        href={`/albums/${album.id}`}
+        class="text-gray-900 font-semibold group-hover:text-green-300"
+      >
+        {album.title}
+      </a>
+    </li>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,59 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { h } from "preact";
+import AlbumCard from "../components/AlbumCard.tsx";
+
+export const handler: Handlers<{albums: any[], query: string}> = {
+  async GET(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+    const filter = query ? `?q=${encodeURIComponent(query)}` : "";
+    const albums = await fetch(`https://jsonplaceholder.typicode.com/albums${filter}`).then((res) => res.json());
+    if (!albums) {
+      return new Response("Albums Not found", { status: 404 });
+    }
+    return ctx.render({
+      albums,
+      query
+    })
+  }
+}
+
+export default function Home(props: PageProps<{albums: any[], query: string}>) {
+  const { albums, query } = props.data;
+
+  return (
+    <div class="p-4 mx-auto max-w-screen-md">
+      <img
+        src="/logo.svg"
+        class="w-32 h-32"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <div class="my-6">
+        <h2 class="text-4xl mb-2 font-black">
+          Welcome to `fresh` albums repository.
+        </h2>
+        <p class="text-lg text-gray-500 font-semibold">You can search for albums by name.</p>
+      </div>
+      <form class = "flex w-full gap-2" method="GET">
+        <input
+          class = "flex-grow w-full shadow-sm focus:outline-none focus:ring-2 focus:border-transparent focus-visible:ring-green-300 focus:ring-green-300 focus:border-green-300 block sm:text-lg border-1 rounded-md p-3"
+          type="text"
+          name="q"
+          value={query}
+          placeholder="Search for albums"
+        />
+        <button
+          type="submit"
+          class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-green-300 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-100 px-10"
+        >
+          Search
+        </button>
+      </form>
+      <ul class="grid sm:grid-cols-2 md:grid-cols-3 mt-8 gap-2">
+        {albums.map((album) => (
+          <AlbumCard album={album} />
+        ))}
+      </ul>
+    </div>
+  );
+}
